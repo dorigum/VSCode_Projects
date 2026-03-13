901c42fa157f6b48a7ff768c96f73196d2375224
Mabaragi <gigig9647@gmail.com>
2026-03-13 10:09:46 +0900

    feature: 카테고리별 메뉴 조회 기능 구현

--- src/service/MenuService.java
+++ src/service/MenuService.java
@@ -1,5 +1,14 @@
 package service;
 
-public interface MenuService {
+import model.Menu;
+import java.util.List;
 
+public interface MenuService {
+    List<Menu> getMenusByCategory(String categoryName);
+
+    List<Menu> getCoffeeMenuList();
+
+    List<Menu> getNonCoffeeMenuList();
+
+    List<Menu> getDesertMenuList();
 }
